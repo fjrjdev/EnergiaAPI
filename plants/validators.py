70965737ee9abe49370e0518a19b8619e1de387b
fjrjdev <thedromemafia@gmail.com>
2023-04-13 20:54:09 -0300

    refactor: cep service moved to plants app

--- plants/validators.py
+++ plants/validators.py
@@ -1,6 +1,6 @@
 from django.utils.translation import gettext as _
 from rest_framework import serializers
-from services.cep_service import cep_service
+from .services.cep_service import cep_service
 
 def validate_cep(cep):
     data = cep_service(cep)
